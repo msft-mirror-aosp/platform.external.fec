791afbe58ff9f55145c4adf632ab8cc9ca6e5686
Sami Tolvanen <samitolvanen@google.com>
2015-09-29 13:26:04 +0100

    external/fec: fix host library on Mac
    Disable LOCAL_SANITIZE := integer on Mac due to compatibility issues.
    
    Change-Id: I6f2ad3fc26a10f78cd19cef3e33ac1ada22e7741

--- Android.mk
+++ Android.mk
@@ -15,7 +15,9 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := libfec_rs_host
 LOCAL_CLANG := true
 LOCAL_CFLAGS := -Wall -O3
+ifeq ($(HOST_OS),linux)
 LOCAL_SANITIZE := integer
+endif
 LOCAL_SRC_FILES := \
 	encode_rs_char.c \
 	decode_rs_char.c \
